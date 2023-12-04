194dfea6cd784cec714e995514652b7406e7e7d1
nearryuu <nath.vetter@laposte.net>
2023-12-04 14:06:46 +0100

    First commit

---
+++ app/src/main/java/com/lorisnath/todo/list/TaskListAdapter.kt
@@ -0,0 +1,37 @@
+package com.lorisnath.todo.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.lorisnath.todo.R
+
+class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
+
+    var currentList: List<String> = emptyList()
+
+    // on utilise `inner` ici afin d'avoir accès aux propriétés de l'adapter directement
+    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private var textView : TextView = itemView.findViewById(R.id.task_title)
+
+        fun bind(taskTitle: String) {
+            // on affichera les données ici
+            textView.text = taskTitle
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return currentList.count()
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+}
---
+++ app/src/main/java/com/lorisnath/todo/list/TaskListFragment.kt
@@ -0,0 +1,63 @@
+package com.lorisnath.todo.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lorisnath.todo.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TaskListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TaskListFragment : Fragment() {
+    private var taskList = listOf("Task 1", "Task 2", "Task 3")
+    private val adapter = TaskListAdapter()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_list, container, false)
+        adapter.currentList = taskList
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler)
+        recyclerView.adapter = adapter
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment TaskListFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            TaskListFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
